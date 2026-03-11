9f7fadd84281ae08a7e6196eec793df972f754e3
Anand Kumar Chaubey <anandchaubey101@gmail.com>
2026-03-11 16:50:15 +0530

    fix railway database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 import { StudentsModule } from './students/students.module';
 import { Student } from './students/entity/student.entity';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
-
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: process.env.PGHOST,
-            port: parseInt(process.env.PGPORT || '5432'),
-            username: process.env.PGUSER,
-            password: process.env.PGPASSWORD,
-            database: process.env.PGDATABASE,
+            url: process.env.DATABASE_URL,
             entities: [Student],
             synchronize: true,
+            ssl: {
+                rejectUnauthorized: false,
+            },
         }),
-
         StudentsModule,
     ],
 })
