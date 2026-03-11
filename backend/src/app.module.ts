55416624b0347ef69f5f8cf55397ec078b1891c2
Anand Kumar Chaubey <anandchaubey101@gmail.com>
2026-03-11 18:25:03 +0530

    fix db config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { StudentsModule } from './students/students.module';
 import { Student } from './students/entity/student.entity';
 
 @Module({
     imports: [
+        ConfigModule.forRoot(),
+
         TypeOrmModule.forRoot({
             type: 'postgres',
-            url: process.env.DATABASE_URL,
+            host: process.env.PGHOST,
+            port: parseInt(process.env.PGPORT || '5432'),
+            username: process.env.PGUSER,
+            password: process.env.PGPASSWORD,
+            database: process.env.PGDATABASE,
             entities: [Student],
             synchronize: true,
-            ssl: {
-                rejectUnauthorized: false,
-            },
         }),
+
         StudentsModule,
     ],
 })
