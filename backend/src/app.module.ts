1d34259b5fca87e369e45ae0652a207916cb0f94
Anand Kumar Chaubey <anandchaubey101@gmail.com>
2026-03-11 16:18:22 +0530

    final working project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { StudentsModule } from './students/students.module';
 import { Student } from './students/entity/student.entity';
 
 @Module({
-imports:[
-TypeOrmModule.forRoot({
-type:'postgres',
-host:'localhost',
-port:5432,
- username: 'postgres',
- password: 'postgres',
- database: 'studentsdb',
-entities:[Student],
-synchronize:true
-}),
-StudentsModule
-]
+    imports: [
+        ConfigModule.forRoot(),
+
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.PGHOST,
+            port: parseInt(process.env.PGPORT || '5432'),
+            username: process.env.PGUSER,
+            password: process.env.PGPASSWORD,
+            database: process.env.PGDATABASE,
+            entities: [Student],
+            synchronize: true,
+        }),
+
+        StudentsModule,
+    ],
 })
-export class AppModule {}
+export class AppModule { }
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,20 +1,23 @@
-const API = "https://students-full-stack-app-production.up.railway.app/students"
+const API =
+    window.location.hostname === "localhost"
+        ? "http://localhost:3000/students"
+        : "https://students-full-stack-app-production.up.railway.app/students";
 
-export const getStudents = () => fetch(API).then(r => r.json())
+export const getStudents = () => fetch(API).then(r => r.json());
 
 export const addStudent = (s) =>
     fetch(API, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(s)
-    })
+        body: JSON.stringify(s),
+    });
 
 export const updateStudent = (id, s) =>
     fetch(`${API}/${id}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(s)
-    })
+        body: JSON.stringify(s),
+    });
 
 export const deleteStudent = (id) =>
-    fetch(`${API}/${id}`, { method: "DELETE" })
+    fetch(`${API}/${id}`, { method: "DELETE" });
