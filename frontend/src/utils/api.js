377426279d3ed31efa85cd7e98e3b8c07d83a88b
Anand Kumar Chaubey <anandchaubey101@gmail.com>
2026-03-11 15:41:16 +0530

    connect frontend to railway backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,20 @@
-const API="http://localhost:3000/students"
+const API = "https://students-full-stack-app-production.up.railway.app/students"
 
-export const getStudents=()=>fetch(API).then(r=>r.json())
-export const addStudent=(s)=>fetch(API,{method:"POST",headers:{'Content-Type':'application/json'},body:JSON.stringify(s)})
-export const updateStudent=(id,s)=>fetch(`${API}/${id}`,{method:"PUT",headers:{'Content-Type':'application/json'},body:JSON.stringify(s)})
-export const deleteStudent=(id)=>fetch(`${API}/${id}`,{method:"DELETE"})
+export const getStudents = () => fetch(API).then(r => r.json())
+
+export const addStudent = (s) =>
+    fetch(API, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(s)
+    })
+
+export const updateStudent = (id, s) =>
+    fetch(`${API}/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(s)
+    })
+
+export const deleteStudent = (id) =>
+    fetch(`${API}/${id}`, { method: "DELETE" })
